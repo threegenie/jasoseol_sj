a85cc2097dc235a60050adc0215bc699e2a4f4bd
threegenie <63702924+threegenie@users.noreply.github.com>
2020-09-20 14:11:54 +0900

    jasoseol lecture 3

--- JasProject/JasProject/settings.py
+++ JasProject/JasProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -104,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATUC_ROOT = os.path.join(BASE_DIR, 'static')
--- JasProject/main/views.py
+++ JasProject/main/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import JssForm
+from .models import Jasoseol
 
 # Create your views here.
+
+def index(request):
+    all_jss = Jasoseol.objects.all()
+    return render(request, 'index.html',{'all_jss':all_jss})
+
+def create(request):
+    if request.method == "POST":
+        filled_form = JssForm(request.POST)
+        if filled_form.is_valid():
+            filled_form.save()
+            return redirect('index')
+
+    jss_form = JssForm()
+    return render(request, 'create.html', {'jss_form':jss_form})
