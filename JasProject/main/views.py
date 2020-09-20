24903395d75ff2c884337071a9abd6777ce0f536
threegenie <63702924+threegenie@users.noreply.github.com>
2020-09-20 14:58:12 +0900

    jasoseol lecture 4

--- JasProject/main/views.py
+++ JasProject/main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import JssForm
 from .models import Jasoseol
+from django.http import Http404
 
 # Create your views here.
 
@@ -17,3 +18,28 @@ def create(request):
 
     jss_form = JssForm()
     return render(request, 'create.html', {'jss_form':jss_form})
+
+def detail(request, jss_id):
+    '''
+    try:
+        my_jss = Jasoseol.objects.get(pk=jss_id)
+    except:
+        raise Http404
+    '''
+    my_jss = get_object_or_404(Jasoseol,pk=jss_id)
+    return render(request, 'detail.html',{'my_jss':my_jss})
+
+def delete(request, jss_id):
+    my_jss = Jasoseol.objects.get(pk=jss_id)
+    my_jss.delete()
+    return redirect('index')
+
+def update(request, jss_id):
+    my_jss = Jasoseol.objects.get(pk=jss_id)
+    jss_form = JssForm(instance=my_jss)
+    if request.method == "POST":
+        updated_form = JssForm(request.POST, instance=my_jss)
+        if updated_form.is_valid():
+            updated_form.save()
+            return redirection('index')
+    return render(request,'create/html', {'jss_form:jss_form'})
